0dbac0fca4b3d11e702ad210bad1969a53903c06
DeeGyi <doreengyimahh@gmail.com>
2024-03-26 22:00:55 -0400

    Added Day 6

---
+++ DAY-6-Intro-to-JS/Intro-to-JS/operators-expressions.js
@@ -0,0 +1,61 @@
+//Arithmetic operations
+let x = 33;
+let y = 15;
+
+//get sum or total '+'
+let sum = x + y;
+console.log("Sum:", sum);
+
+
+//get difference '-'
+let difference = x - y;
+console.log("Difference:", difference);
+
+//get product '*'
+let product = x * y;
+console.log("Product:", product);
+
+//get quotient '/'
+let quotient = x / y;
+console.log("Quotient:", quotient);
+
+//get modulus '%'
+let remainder = x % y;
+console.log("Remainder:", remainder);
+
+
+// Assignment expressions
+let a = 10;
+let b = 5;
+
+//  Addition '=+'
+a += b;
+console.log("a += b is:", a);
+
+// Substraction '-='
+
+a -= b;
+console.log("a -= b is:", a);
+
+// Multiplication '*='
+
+a *= b;
+console.log("a *= b is:", a);
+
+// Division '/='
+
+a /= b;
+console.log("a /= b is:", a);
+
+//Concatenation
+const firstWord = "Hello";
+const secondWord = "World!";
+console.log(firstWord + " " + secondWord);
+
+//Logical Expression
+let sunny = true;
+let warm = false;
+
+onsole.log("Is it sunny AND warm?", sunny && warm);
+console.log("Is it sunny OR warm?", sunny || warm);
+console.log("Is it NOT sunny?", !sunny)
--- Day-5-Intermediate-CSS/intro-to-javascript/index.js
+++ Day-5-Intermediate-CSS/intro-to-javascript/index.js
@@ -1,11 +1,16 @@
+// 1. Should be meaningful and descriptive.
+// 2. Should not start with a number.
+// 3. Should not contain a space or hyphen.
+// 4. Case-sensitive
+// 5. Cannot use reserved keywords
+
+
+// Variables
+// let Variables
 let firstName = "John";
 // let FirstName = "Blossom";
 
 console.log('Hello!', firstName);
 // console.log('Hello!', FirstName);
 
-// 1. Should be meaningful and descriptive.
-// 2. Should not start with a number.
-// 3. Should not contain a space or hyphen.
-// 4. Case-sensitive
-// 5. Cannot ue reserved keywords
+
